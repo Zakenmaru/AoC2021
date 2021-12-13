f8021c546f278cd3966f149c420958fdf4cff621
unknown <sohammukherjee2000@gmail.com>
2021-12-12 20:13:59 -0600

    d3p2 incomplete

--- D3/main.py
+++ D3/main.py
@@ -9,11 +9,11 @@ def create_array():
     for i in range(row_len):
         l = [a[i] for a in arr]
         dig.append(l)
-    return dig
+    return dig, arr
 
 
 def p1():
-    dig = create_array()
+    dig, _ = create_array()
     gamma = ""
     for i in dig:
         gamma += "1" if i.count("1") > i.count("0") else "0"
@@ -25,8 +25,34 @@ def p1():
     print(res)
 
 
+def p2():
+    dig, arr = create_array()
+    temp_arr_o = temp_arr_co = arr
+    index = 0
+    for i in dig:
+        while len(temp_arr_o) > 1 and len(temp_arr_co) > 1:
+            zero_count = i.count("0")
+            one_count = i.count("1")
+            oxygen_one = one_count >= zero_count
+            for bin in arr:
+                if oxygen_one:
+                    if (bin[index] == "1"):
+                        temp_arr_o.remove(bin)
+                    else:
+                        temp_arr_co.remove(bin)
+                else:
+                    if (bin[index] == "0"):
+                        temp_arr_o.remove(bin)
+                    else:
+                        temp_arr_co.remove(bin)
+            index += 1
+    
+    res = int(temp_arr_o[0], 2) * int(temp_arr_co[0], 2)
+    print(res)
+
 def main():
     p1()
+    p2()
 
 
 if __name__ == '__main__':
