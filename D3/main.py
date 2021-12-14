868909154304d92a6fcd8efacc6e7a51cee48b59
unknown <sohammukherjee2000@gmail.com>
2021-12-14 14:04:03 -0600

    d3p2

--- D3/main.py
+++ D3/main.py
@@ -1,3 +1,5 @@
+import copy
+
 filename = "inp.txt"
 
 def create_array():
@@ -27,25 +29,22 @@ def p1():
 
 def p2():
     dig, arr = create_array()
-    temp_arr_o = temp_arr_co = arr
+    temp_arr_o = copy.copy(arr)
+    temp_arr_co = copy.copy(arr)
     index = 0
     for i in dig:
-        while len(temp_arr_o) > 1 and len(temp_arr_co) > 1:
-            zero_count = i.count("0")
-            one_count = i.count("1")
-            oxygen_one = one_count >= zero_count
-            for bin in arr:
-                if oxygen_one:
-                    if (bin[index] == "1"):
-                        temp_arr_o.remove(bin)
-                    else:
-                        temp_arr_co.remove(bin)
-                else:
-                    if (bin[index] == "0"):
-                        temp_arr_o.remove(bin)
-                    else:
-                        temp_arr_co.remove(bin)
-            index += 1
+        if len(temp_arr_o) == 1 and len(temp_arr_o) == 1 and index >= len(dig):
+            break
+        mcv = "1" if i.count("1") >= i.count("0") else "0"
+        lcv = "0" if i.count("0") <= i.count("1") else "1"
+        for bin in arr:
+            if bin[index] != mcv:
+                temp_arr_o.remove(bin)
+                arr.remove(bin)
+            if bin[index] != lcv:
+                temp_arr_co.remove(bin)
+                arr.remove(bin)
+        index += 1
     
     res = int(temp_arr_o[0], 2) * int(temp_arr_co[0], 2)
     print(res)
